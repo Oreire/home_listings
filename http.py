fa5ee83a251efd43c2091385c5d455c3a3b0d00f
Ayomide Ajayi <lancerajayi@yahoo.com>
2025-02-06 00:57:26 +0000

    python chnages

--- http.py
+++ http.py
@@ -1,18 +1,25 @@
 import requests
-import boto3
-import numpy 
 
-# URL to connect to
-url = "http://www.google.com"
+# List of URLs to connect to
+urls = [
+    "http://www.google.com",
+    "http://www.example.com",
+    "http://www.github.com"
+]
 
-
-# Make a GET request to fetch data
-response = requests.get(url)
-
-# Check if the connection is successful
-if response.status_code == 200:
-    print("Successfully connected to the internet.")
-    print("Response content:")
-    print(response.text)  # Printing the HTML content of the page
-else:
-    print("Failed to connect to the internet.")
+# Iterate over each URL in the list
+for url in urls:
+    # Make a GET request to fetch data
+    try:
+        response = requests.get(url)
+        
+        # Check if the connection is successful
+        if response.status_code == 200:
+            print(f"Successfully connected to {url}.")
+            print("Response content:")
+            print(response.text)  # Printing the HTML content of the page
+        else:
+            print(f"Failed to connect to {url}.")
+            print("Status code:", response.status_code)
+    except requests.exceptions.RequestException as e:
+           print(f"An error occurred while trying to connect to {url}: {e}")
